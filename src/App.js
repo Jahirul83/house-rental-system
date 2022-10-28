ad33a0969cd9265490150c92759b7ec0ab02aa02
Jahirul Islam <jahirulislamcse83@gmail.com>
2022-10-28 15:28:44 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          cholo <code>src/App.js</code> diya project kori.
         </p>
         <a
           className="App-link"
