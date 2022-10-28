953a28760de0373b1eee9e9b9143fffe84503eee
Jahirul Islam <jahirulislamcse83@gmail.com>
2022-10-28 19:47:49 +0600

    Navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,42 @@
+import { MenuItems } from "./MenuItems"
+import React, { Component } from "react";
+import './Navbar.css';
+import { Button } from "../Button";
+class Navbar extends Component {
+    state={ clicked: false }
+
+    handleClick = () =>{
+        this.setState({ clicked: !this.state.clicked})
+    }
+
+
+
+    render(){
+        return(
+            <nav className="NavbarItems">
+                <h1 className="navbar-logo">
+                    React<i className="fab fa-react"></i>
+                </h1>
+                <div className="manu-icon" onClick={this.handleClick}>
+                    <i className={this.state.clicked ? "fa-solid fa-x":"fa-solid fa-bars"}></i>
+                </div>
+                <ui className={this.state.clicked ? "nav-menu active" : "nav-manu"}>
+                    {MenuItems.map((item,index)=>
+                    {
+                        return (
+                            <li key={index}>
+                                <a className={item.cName} href={item.url}>
+                                {item.title}
+                                </a>
+                            </li>
+                        )
+                    }
+                    )}
+                    
+                </ui>
+                <Button>Sign Up</Button>
+            </nav>
+        )
+    }
+}
+export default Navbar
