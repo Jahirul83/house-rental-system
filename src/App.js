35adcdf080d5c2a3ad105836939abe1c9326f39a
Jahirul Islam <jahirulislamcse83@gmail.com>
2022-11-14 22:25:33 +0600

    Home page banner

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar/Navbar';
 import './App.css';
 import Footer from "./Footer";
+import HomePage from "./pages/HomePage";
 
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <HomePage/>
       <Footer/>
     </div>
   );
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -15,7 +15,7 @@ class Navbar extends Component {
         return(
             <nav className="NavbarItems">
                 <h1 className="navbar-logo">
-                    Rent<i className="fab fa-react"></i>
+                    <i class="fa-solid fa-house"></i>Rent
                 </h1>
                 <div className="manu-icon" onClick={this.handleClick}>
                     <i className={this.state.clicked ? "fa-solid fa-x":"fa-solid fa-bars"}></i>
