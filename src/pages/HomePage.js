cedf7e8465a6601736ae17e346096cf5a709a887
Jahirul83 <jahirulislamcse83.com>
2022-12-24 16:48:22 +0600

    card added to home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,64 +1,68 @@
 import React from "react";
 import './HomePage.css';
+import Services from "./Services";
 //test
 const HomePage = () => {
-    return ( 
-       <section id="page-banner">
-    <div className="container-fluid">
-    <div className="row">
-        <div className="col-sm-12">
-            <div className="home-banner-search">
+    return (
+        <section id="page-banner">
+            <div className="container-fluid">
                 <div className="row">
-                    <div className="container">
-                        <div className="row align-self-center justify-content-center">
-                            <div className="col-sm-12 col-md-9 col-lg-10 col-xl-10 col-xxl-8">
-                                <h2 className="text-center">Search properties for sale and for rent in Bangladesh</h2>
-                                <form method="POST" className="home-search-form" id="home-search-form">
-                                    <input type="hidden" name="_token"/>                                    
-                                    <div className="row justify-content-center">
-                                        <div className="col-sm-3 form-group">
-                                            <input type="text" name="address" className="form-control place-autocomplete" placeholder="Address, City, Zip"/>
-                                        </div>
-                                        <div className="col-sm-3 form-group">
-                                            <select name="property_purpose_id" className="form-select" >
-                                                <option value="">Any</option>
-                                                <option  value="1">For Rent</option>
-                                                <option  value="2">For Sale</option>
-                                            </select>
-                                        </div>
-                                        <div className="col-sm-3 form-group">
-                                            <select name="property_type_id" className="form-select">
-                                                <option value="">Any</option>
-                                                <option  value="1">Apartment/Flats</option>
-                                                <option  value="2">Independent House</option>
-                                                <option  value="3">Duplex Home</option>
-                                                <option  value="4">Studio Apartment</option>
-                                                <option  value="5">Sublet/Room</option>
-                                                <option  value="6">Guest House</option>
-                                                <option  value="7">Plot</option>
-                                                <option  value="8">Showroom / Shop / Restaurant</option>
-                                                <option  value="9">Office space</option>
-                                                <option  value="10">Agriculture / Farm Land</option>
-                                                <option  value="11">Industrial Space</option>
-                                            </select>
-                                        </div>
-                                        <div className="col-sm-2 d-grid form-group">
-                                            <button className="btn btn-primary btn-block" id="place-search-btn" type="submit">Search</button>
-                                           
+                    <div className="col-sm-12">
+                        <div className="home-banner-search">
+                            <div className="row">
+                                <div className="container">
+                                    <div className="row align-self-center justify-content-center">
+                                        <div className="col-sm-12 col-md-9 col-lg-10 col-xl-10 col-xxl-8">
+                                            <h2 className="text-center">Search properties for sale and for rent in Bangladesh</h2>
+                                            <form method="POST" className="home-search-form" id="home-search-form">
+                                                <input type="hidden" name="_token" />
+                                                <div className="row justify-content-center">
+                                                    <div className="col-sm-3 form-group">
+                                                        <input type="text" name="address" className="form-control place-autocomplete" placeholder="Address, City, Zip" />
+                                                    </div>
+                                                    <div className="col-sm-3 form-group">
+                                                        <select name="property_purpose_id" className="form-select" >
+                                                            <option value="">Any</option>
+                                                            <option value="1">For Rent</option>
+                                                            <option value="2">For Sale</option>
+                                                        </select>
+                                                    </div>
+                                                    <div className="col-sm-3 form-group">
+                                                        <select name="property_type_id" className="form-select">
+                                                            <option value="">Any</option>
+                                                            <option value="1">Apartment/Flats</option>
+                                                            <option value="2">Independent House</option>
+                                                            <option value="3">Duplex Home</option>
+                                                            <option value="4">Studio Apartment</option>
+                                                            <option value="5">Sublet/Room</option>
+                                                            <option value="6">Guest House</option>
+                                                            <option value="7">Plot</option>
+                                                            <option value="8">Showroom / Shop / Restaurant</option>
+                                                            <option value="9">Office space</option>
+                                                            <option value="10">Agriculture / Farm Land</option>
+                                                            <option value="11">Industrial Space</option>
+                                                        </select>
+                                                    </div>
+                                                    <div className="col-sm-2 d-grid form-group">
+                                                        <button className="btn btn-primary btn-block" id="place-search-btn" type="submit">Search</button>
+
+                                                    </div>
+                                                </div>
+                                            </form>
                                         </div>
                                     </div>
-                                </form>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
-</div>
-                </section>
-  );
-  
+            <div>
+                <Services/>
+            </div>
+        </section>
+    );
+
 };
 
 export default HomePage;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -9,34 +9,34 @@ const Login = () => {
   return (
     <div className="logincard">
       <Card className="cardDesign mt-5 mb-5" >
-      <Card.Body>
-      <Form>
-      <Form.Group className="mb-3 " controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
+        <Card.Body>
+          <Form>
+            <Form.Group className="mb-3 " controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <div class="col-md-12 text-center">
-        <Button  variant="success" type="submit">
-           Login
-       </Button>
-      </div>
-      <Form.Text>
-          Don't have an account? Please <Link to='/SignUp'>Sign Up</Link>
-        </Form.Text>
-    </Form>
-      </Card.Body>
-    </Card>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <div class="col-md-12 text-center">
+              <Button variant="success" type="submit">
+                Login
+              </Button>
+            </div>
+            <Form.Text>
+              Don't have an account? Please <Link to='/SignUp'>Sign Up</Link>
+            </Form.Text>
+          </Form>
+        </Card.Body>
+      </Card>
     </div>
   )
 }
