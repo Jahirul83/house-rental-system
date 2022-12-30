aeb9ab2eccfa7c2d19fc7777bfeae51b1f38783c
Jahirul83 <jahirulislamcse83.com>
2022-12-31 02:22:25 +0600

    Review card done

--- src/components/Cards/ReviewCard.js
+++ src/components/Cards/ReviewCard.js
@@ -8,14 +8,34 @@ const ReviewCard = ({ review }) => {
 
     return (
         <div>
-            <Card className='review-card-design'>
+            <Card className='review-card-design' style={{higt:"500px" , width:"30vw"}}>
                 <Card.Body>
-                    <Card.Title>{review.name}</Card.Title>
-                    <Card.Subtitle className="mb-2">Card Subtitle</Card.Subtitle>
-                    <Card.Text>
+                    <Card.Title className='d-flex mt-2'>
+                        <div>
+                            <img src="https://aatapi.org/wp-content/uploads/2020/02/user.jpg" style={{ width: "80px" }} alt="" />
+                        </div>
+                        <div className='mt-4'>
+                            {review.name}
+                        </div>
+                    </Card.Title>
+                    <Card.Text className='mb-3'>
                         {review.review}
                     </Card.Text>
+
                 </Card.Body>
+                <div class="row mb-3 ms-2">
+                    <div class="col-8">
+                        <i class="far fa-thumbs-up"></i><span>{' '}</span>
+                        <i class="far fa-comment-dots"></i>
+                    </div>
+                    <div class="col-4">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star-half-alt"></i>
+                    </div>
+                </div>
+
             </Card>
         </div>
     );
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,17 +2,19 @@ import React, { useEffect, useState } from "react";
 import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Cards from "../components/Cards/Cards";
+import ReviewCard from "../components/Cards/ReviewCard";
 import Banner from "./Banner/Banner";
 import FAQ from "./FAQ/FAQ";
 import './HomePage.css';
 import Services from "./Services";
 import ShowReview from "./ShowReview/ShowReview";
-//test
 const HomePage = (props) => {
 
     const [services, setService] = useState([]);
+    const [showReview, setShowReview] = useState([]);
 
     const selectedServices = services?.slice(0, 3);
+    const selectedReviews = showReview?.slice(0, 3);
 
     useEffect(() => {
         fetch('http://localhost:5000/services')
@@ -20,60 +22,12 @@ const HomePage = (props) => {
             .then(data => setService(data));
     }, []);
 
+    useEffect(() => {
+        fetch('http://localhost:5000/addreview')
+            .then(res => res.json())
+            .then(data => setShowReview(data));
+    }, []);
     return (
-        // <section id="page-banner">
-        //     <div className="container-fluid">
-        //         <div className="row">
-        //             <div className="col-sm-12">
-        //                 <div className="home-banner-search">
-        //                     <div className="row">
-        //                         <div className="container">
-        //                             <div className="row align-self-center justify-content-center">
-        //                                 <div className="col-sm-12 col-md-9 col-lg-10 col-xl-10 col-xxl-8">
-        //                                     <h2 className="text-center">Search properties for sale and for rent in Bangladesh</h2>
-        //                                     <form method="POST" className="home-search-form" id="home-search-form">
-        //                                         <input type="hidden" name="_token" />
-        //                                         <div className="row justify-content-center">
-        //                                             <div className="col-sm-3 form-group">
-        //                                                 <input type="text" name="address" className="form-control place-autocomplete" placeholder="Address, City, Zip" />
-        //                                             </div>
-        //                                             <div className="col-sm-3 form-group">
-        //                                                 <select name="property_purpose_id" className="form-select" >
-        //                                                     <option value="">Any</option>
-        //                                                     <option value="1">For Rent</option>
-        //                                                     <option value="2">For Sale</option>
-        //                                                 </select>
-        //                                             </div>
-        //                                             <div className="col-sm-3 form-group">
-        //                                                 <select name="property_type_id" className="form-select">
-        //                                                     <option value="">Any</option>
-        //                                                     <option value="1">Apartment/Flats</option>
-        //                                                     <option value="2">Independent House</option>
-        //                                                     <option value="3">Duplex Home</option>
-        //                                                     <option value="4">Studio Apartment</option>
-        //                                                     <option value="5">Sublet/Room</option>
-        //                                                     <option value="6">Guest House</option>
-        //                                                     <option value="7">Plot</option>
-        //                                                     <option value="8">Showroom / Shop / Restaurant</option>
-        //                                                     <option value="9">Office space</option>
-        //                                                     <option value="10">Agriculture / Farm Land</option>
-        //                                                     <option value="11">Industrial Space</option>
-        //                                                 </select>
-        //                                             </div>
-        //                                             <div className="col-sm-2 d-grid form-group">
-        //                                                 <button className="btn btn-primary btn-block" id="place-search-btn" type="submit">Search</button>
-
-        //                                             </div>
-        //                                         </div>
-        //                                     </form>
-        //                                 </div>
-        //                             </div>
-        //                         </div>
-        //                     </div>
-        //                 </div>
-        //             </div>
-        //         </div>
-        //     </div>
         <div>
             <div>
                 <Banner />
@@ -93,11 +47,21 @@ const HomePage = (props) => {
                     <div className="d-flex justify-content-center">
                         <Link to="/services"><Button variant="outline-primary btn-lg">Show More</Button></Link>
                     </div>
+                    <div className='d-flex flex-wrap gap-5 justify-content-center my-4'>
+                        {
+                            selectedReviews.map(review =>
+                                <ReviewCard
+                                    key={review._id}
+                                    review={review}
+                                ></ReviewCard>
+                            )
+                        }
+                    </div>
                     <div className="d-flex justify-content-center">
-                        <ShowReview/>
+                        <Link to="/showreview"><Button variant="outline-primary btn-lg">Show More</Button></Link>
                     </div>
                     <div>
-                        <FAQ/>
+                        <FAQ />
                     </div>
                 </div>
             </div>
